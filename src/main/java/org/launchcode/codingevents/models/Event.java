5c2e2e312d2e9d1aa8bbf4945825b28baefc0d95
lmbrumleve <142369731+lmbrumleve@users.noreply.github.com>
2024-01-24 16:51:10 -0600

    model validation ch 15 text examples

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,5 +1,9 @@
 package org.launchcode.codingevents.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 import java.util.Objects;
 
 /**
@@ -10,16 +14,26 @@ public class Event {
     private int id;
     private static int nextId = 1;
 
+    @NotBlank(message = "Name is required.")
+    @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters.")
     private String name;
+
+    @Size(max = 500, message = "Description too long.")
     private String description;
 
-    public Event(String name, String description) {
+    @NotBlank(message = "Email is required.")
+    @Email(message = "Invalid email. Try again.")
+    private String contactEmail;
+
+    public Event(String name, String description, String contactEmail) {
         this.name = name;
         this.description = description;
+        this.contactEmail = contactEmail;
         this.id = nextId;
         nextId++;
     }
 
+    public Event() {}
     public String getName() {
         return name;
     }
@@ -30,6 +44,14 @@ public class Event {
 
     public String getDescription() {
         return description;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
     }
 
     public void setDescription(String description) {
